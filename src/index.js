8f479ebd0ce3bc47d5f484c529f21e9ad27ce0c1
Fernando <fernandorato.0@hotmail.com>
2019-11-17 20:35:00 -0300

    Criado ambiente com Eslint Prettier

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function app() {
+    return (
+        <View>
+            <Text>Olá</Text>
+        </View>
+    );
+}
